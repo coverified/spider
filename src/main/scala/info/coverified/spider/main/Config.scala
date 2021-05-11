c5f1afd607d0063fedda3b26b840d39adedd0cb9
Johannes Hiry <johannes.hiry@tu-dortmund.de>
2021-05-11 13:04:57 +0200

    adapted default values in Config

--- src/main/scala/info/coverified/spider/main/Config.scala
+++ src/main/scala/info/coverified/spider/main/Config.scala
@@ -29,7 +29,7 @@ object Config extends LazyLogging {
   // all time values in milliseconds
   private val defaultParams: Map[String, Int] = Map(
     SCRAPE_PARALLELISM -> 100,
-    SCRAPE_INTERVAL -> 100,
+    SCRAPE_INTERVAL -> 1000,
     SCRAPE_TIMEOUT -> 5000,
     SHUTDOWN_TIMEOUT -> 15000
   )
