c8697678a47240c6d0a9e62f350fd733efab3316
Sebastian <8848001-sebastian-peter@users.noreply.gitlab.com>
2021-06-02 18:05:46 +0200

    SiteScraperSpec

---
+++ src/test/scala/info/coverified/spider/SiteScraperSpec.scala
@@ -0,0 +1,146 @@
+/**
+ * © 2021. CoVerified,
+ * Diehl, Fetzer, Hiry, Kilian, Mayer, Schlittenbauer, Schweikert, Vollnhals, Weise GbR
+ **/
+
+package info.coverified.spider
+
+import akka.actor.testkit.typed.scaladsl.BehaviorTestKit
+import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.client.WireMock
+import com.github.tomakehurst.wiremock.client.WireMock.{status, urlEqualTo}
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig
+import info.coverified.spider.SiteScraper.SiteContent
+import org.scalatest.BeforeAndAfterAll
+
+import java.net.{URL, UnknownHostException}
+import scala.concurrent.duration.DurationInt
+
+class SiteScraperSpec extends ActorSpec with BeforeAndAfterAll {
+
+  // pick a random free port
+  val wireMockServer = new WireMockServer(
+    wireMockConfig().dynamicPort().dynamicHttpsPort()
+  )
+
+  private def port: Int = wireMockServer.port()
+
+  override def beforeAll(): Unit = {
+    wireMockServer.start()
+    super.beforeAll()
+  }
+
+  override def afterAll(): Unit = {
+    wireMockServer.stop()
+    super.afterAll()
+  }
+
+  "Scrape message received by SiteScraper" should {
+    "result in indexation and extraction of URL if status code is ok" in {
+      // set up test page
+      val html = """|<html>
+                    |  <head></head>
+                    |  <body>
+                    |    <a href="/page2">Link to page 2</a>
+                    |  </body>
+                    |</html>""".stripMargin
+
+      wireMockServer.stubFor(
+        WireMock
+          .get(urlEqualTo("/page1"))
+          .willReturn(
+            status(200)
+              .withHeader("Content-Type", "text/html")
+              .withBody(html)
+          )
+      )
+
+      val indexer =
+        testKit.createTestProbe[Indexer.IndexerEvent]("Indexer_127.0.0.1")
+      val hostCrawler = testKit.createTestProbe[HostCrawler.HostCrawlerEvent](
+        "Scraper_127.0.0.1"
+      )
+
+      val behaviorKit: BehaviorTestKit[SiteScraper.SiteScraperEvent] =
+        BehaviorTestKit(
+          SiteScraper(
+            indexer.ref,
+            defaultConfig.scrapeTimeout
+          )
+        )
+
+      val pageUrl = new URL(s"http://127.0.0.1:$port/page1")
+      behaviorKit.run(SiteScraper.Scrape(pageUrl, hostCrawler.ref))
+
+      indexer.expectMessage(
+        Indexer.Index(
+          pageUrl,
+          SiteContent(Set(new URL(s"http://127.0.0.1:$port/page2")))
+        )
+      )
+      hostCrawler.expectNoMessage()
+    }
+
+    "result in no indexation if status code is not ok" in {
+      // set up test page
+      wireMockServer.stubFor(
+        WireMock
+          .get(urlEqualTo("/page2"))
+          .willReturn(
+            status(404)
+              .withHeader("Content-Type", "text/html")
+          )
+      )
+
+      val indexer =
+        testKit.createTestProbe[Indexer.IndexerEvent]("Indexer_127.0.0.1")
+      val hostCrawler = testKit.createTestProbe[HostCrawler.HostCrawlerEvent](
+        "Scraper_127.0.0.1"
+      )
+
+      val behaviorKit: BehaviorTestKit[SiteScraper.SiteScraperEvent] =
+        BehaviorTestKit(
+          SiteScraper(
+            indexer.ref,
+            2.seconds
+          )
+        )
+
+      val pageUrl = new URL(s"http://127.0.0.1:$port/page2")
+      behaviorKit.run(SiteScraper.Scrape(pageUrl, hostCrawler.ref))
+
+      indexer.expectMessage(
+        Indexer.NoIndex(pageUrl)
+      )
+      hostCrawler.expectNoMessage()
+    }
+
+    "result in failure message sent to HostCrawler when given host is unreachable" in {
+      val indexer =
+        testKit.createTestProbe[Indexer.IndexerEvent]("Indexer_127.0.0.1")
+      val hostCrawler = testKit.createTestProbe[HostCrawler.HostCrawlerEvent](
+        "Scraper_127.0.0.1"
+      )
+
+      val behaviorKit: BehaviorTestKit[SiteScraper.SiteScraperEvent] =
+        BehaviorTestKit(
+          SiteScraper(
+            indexer.ref,
+            1.second
+          )
+        )
+
+      // request impossible URL
+      val pageUrl = new URL(s"http://127.0.0.0.0.0.0.0.1:$port")
+      behaviorKit.run(SiteScraper.Scrape(pageUrl, hostCrawler.ref))
+
+      indexer.expectNoMessage()
+      val failureMsg =
+        hostCrawler.expectMessageType[HostCrawler.SiteScrapeFailure]
+      failureMsg.url shouldBe pageUrl
+      // UnknownHostException doesn't implement equals(), so we check ourselves
+      failureMsg.reason.getClass shouldBe classOf[UnknownHostException]
+    }
+  }
+
+}
